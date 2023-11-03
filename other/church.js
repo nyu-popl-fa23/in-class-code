93d2b6d9b959b3775e65eac90f5283304af037ff
Thomas Wies <wies@cs.nyu.edu>
2023-11-02 22:38:03 -0400

    working version of Church encodings

--- other/church.js
+++ other/church.js
@@ -6,57 +6,57 @@
 
 // fix(f) -> (x => f(x(x)))(x => f(x(x))) -> f((x => f(x(x)))(x => f(x(x)))) = f(fix(f))
 
-const fix = f => (x => f(n => x(x)(n)))(x => f(n => x(x)(n)))
+const fix = f => (x => f(n => x(x)(n)))(x => f(n => x(x)(n)));
 
 /*
 function fac(n) {
   return n == 0 ? 1 : n * fac(n - 1)
 }*/
 
-const ctrue = x => y => x
-const cfalse = x => y => y
+const ctrue = x => y => x;
+const cfalse = x => y => y;
 
-const ite = b => x => y => b(x)(y)
+const ite = b => x => y => b(x)(y)(x);
 
-const and = a => b => a(b)(a)
+const and = a => b => a(b)(a);
 
-const or = a => b => a(a)(b)
+const or = a => b => a(a)(b);
 
-const not = b => b(cfalse)(ctrue)
+const not = b => b(cfalse)(ctrue);
 
-const zero = s => z => z
-const one = s => z => s(z)
-const two = s => z => s(s(z))
+const zero = s => z => z;
+const one = s => z => s(z);
+const two = s => z => s(s(z));
 
-const fromNumFun = fromNum => n => s => z => n == 0 ? z : s(fromNum(n - 1)(s)(z)) 
+const fromNumFun = fromNum => n => s => z => n === 0 ? z : s(fromNum(n - 1)(s)(z));
 
-const fromNum = fix(fromNumFun)
+const fromNum = fix(fromNumFun);
 
-const toNum = cn => cn(n => n + 1)(0)
+const toNum = cn => cn(n => n + 1)(0);
 
-const plusOne = cn => s => z => s(cn(s)(z))
+const plusOne = cn => s => z => s(cn(s)(z));
 
-const plus = cn => cm => cn(plusOne)(cm)
+const plus = cn => cm => cn(plusOne)(cm);
 
-const mult = cn => cm => cn(plus(cm))(zero)
+const mult = cn => cm => cn(plus(cm))(zero);
 
-const isZero = cn => cn(b => cfalse)(ctrue)
+const isZero = cn => cn(b => cfalse)(ctrue);
 
-const pair = x => y => b => b(x)(y)
+const pair = x => y => b => b(x)(y);
 
-const fst = p => p(ctrue)
+const fst = p => p(ctrue);
 
-const snd = p => p(cfalse)
+const snd = p => p(cfalse);
 
-const minusOne = cn => snd(cn(p => pair(plusOne(fst(p)))(fst(p)))(pair(zero)(zero)))
+const minusOne = cn => snd(cn(p => pair(plusOne(fst(p)))(fst(p)))(pair(zero)(zero)));
 
-const res = toNum(minusOne(fromNum(5)))
+const res = toNum(minusOne(fromNum(5)));
 
-res
+console.log(res);
 
 // const x = ed; eb === (x => eb)(ed)
 
-const facFun = fac => n => ite(isZero(n))(one)(mult(n)(fac(minusOne(n))))
+const facFun = fac => n => ite(isZero(n))(x => one)(x => mult(n)(fac(minusOne(n))));
 
 /*const fac0 = n => 0
 const fac1 = facFun(fac0)
@@ -68,8 +68,8 @@ const fac6 = facFun(fac5)*/
 
 // fac = facFun(fac)
 
-const fac = fix(facFun)
+const fac = fix(facFun);
 
-const result = toNum(fac(fromNum(5)))
+const result = toNum(fac(fromNum(5)));
 
-result
+console.log(result)
